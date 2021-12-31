f0a062e9544ccff72389dbed8648572002b80f22
Madani Khoucha <madani.kh5@gmail.com>
2021-12-31 14:26:50 +0100

    last version of ocr project

--- projet_ocr.py
+++ projet_ocr.py
@@ -4,6 +4,10 @@ from PyQt5.QtGui import QImage
 import cv2 as cv
 import numpy as np
 import imutils
+from skimage import img_as_ubyte,img_as_int
+from skimage.filters import (threshold_otsu,
+                             threshold_sauvola)
+
 
 
 
@@ -29,10 +33,41 @@ class Ui_MainWindow(object):
         self.horizontalLayout.addWidget(self.pushButton)
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_4.setVisible(False)
+        self.horizontalLayout.addWidget(self.pushButton_4)
         self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_3.setObjectName("pushButton_3")
         self.horizontalLayout.addWidget(self.pushButton_2)
         self.verticalLayout_2.addLayout(self.horizontalLayout)
+
+
+        self.comboBox_4 = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox_4.setObjectName("comboBox_4")
+        self.verticalLayout_2.addWidget(self.comboBox_4)
+
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_3.setContentsMargins(-1, 16, -1, -1)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.verticalLayout_2.addLayout(self.verticalLayout_3)
+
+        self.comboBox_5 = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox_5.setObjectName("comboBox_5")
+        self.comboBox_5.setVisible(False)
+        self.verticalLayout_3.addWidget(self.comboBox_5)
+
+        self.horizontalSlider = QtWidgets.QSlider(self.centralwidget)
+        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider.setObjectName("horizontalSlider")
+        self.verticalLayout_3.addWidget(self.horizontalSlider)
+
+        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_5.setVisible(False)
+        self.verticalLayout_3.addWidget(self.pushButton_5)
+        
+
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setContentsMargins(-1, 16, -1, -1)
         self.verticalLayout.setObjectName("verticalLayout")
@@ -50,10 +85,7 @@ class Ui_MainWindow(object):
         self.comboBox_3.setVisible(False)
         self.horizontalLayout_2.addWidget(self.comboBox_3)
         self.verticalLayout.addLayout(self.horizontalLayout_2)
-        self.horizontalSlider = QtWidgets.QSlider(self.centralwidget)
-        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
-        self.horizontalSlider.setObjectName("horizontalSlider")
-        self.verticalLayout.addWidget(self.horizontalSlider)
+        
         self.horizontalSlider_2 = QtWidgets.QSlider(self.centralwidget)
         self.horizontalSlider_2.setOrientation(QtCore.Qt.Horizontal)
         self.horizontalSlider_2.setObjectName("horizontalSlider_2")
@@ -75,13 +107,31 @@ class Ui_MainWindow(object):
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
+        # cacher tous les widgets
+        self.comboBox.setVisible(False)
+        self.comboBox.setVisible(False)
+        self.comboBox.setVisible(False)
+        self.horizontalSlider_2.setVisible(False)
+        self.pushButton_3.setVisible(False)
+
         self.retranslateUi(MainWindow)
         self.horizontalSlider.setVisible(False)
         self.horizontalSlider.valueChanged['int'].connect(self.th_value)
         self.horizontalSlider_2.valueChanged['int'].connect(self.filter_value)
         self.pushButton.clicked.connect(self.loadImage)
         self.pushButton_2.clicked.connect(self.saveImage)
+        self.pushButton_4.clicked.connect(self.rechargeOriginalImg)
         self.pushButton_3.clicked.connect(self.confirmeFiltre)
+        self.pushButton_5.clicked.connect(self.confirmeBinarization)
+
+
+        actions = ["Binarisation", "Filtrage"]
+        self.comboBox_4.addItem("Selectionner une action")
+        self.comboBox_4.addItems(actions)
+
+        binarisations = ["OTSU","Threshold","Sauvola"]
+        self.comboBox_5.addItem("Selectionner une technique de binarisation")
+        self.comboBox_5.addItems(binarisations)
 
         filtres = ["filtre moyen", "filtre mediane","filtre gaussien","filtre laplacien","filtres morphologiques"]
         self.comboBox.addItem("Selectionner un filtre")
@@ -98,7 +148,8 @@ class Ui_MainWindow(object):
         self.comboBox.activated[str].connect(self.choiceFilter)
         self.comboBox_2.activated[str].connect(self.choiceMorph)
         self.comboBox_3.activated[str].connect(self.choiceKernel)
-
+        self.comboBox_4.activated[str].connect(self.choiceAction)
+        self.comboBox_5.activated[str].connect(self.choiceBinarization)
 
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
@@ -108,13 +159,19 @@ class Ui_MainWindow(object):
         self.blur = 0
         # self.threshold = None
 
+        self.action_val = None
+        self.binarisation_val = None
         self.filter_val = None
         self.morph_val = None 
         self.kernel_val = None
+
+
+    def choiceBinarization(self,value):
+        self.binarisation_val = value
+        self.horizontalSlider.setSliderPosition(0)
     
     def choiceFilter(self, value):
         self.filter_val = value
-        print("filter : ",value)
         if(self.filter_val != "filtres morphologiques"):
             self.comboBox_2.setVisible(False)
             self.comboBox_3.setVisible(False)
@@ -130,7 +187,30 @@ class Ui_MainWindow(object):
             # self.horizontalSlider.setVisible(False)
             self.horizontalSlider_2.setVisible(True)
 
+    def choiceAction(self,value):
+            # self.setPhoto(self.image)
+            self.action_val = value
+            if(self.action_val == "Binarisation"):
+                self.setPhoto(cv.cvtColor(self.image,cv.COLOR_BGR2GRAY))
+                self.pushButton_5.setVisible(True)
+                self.comboBox.setVisible(False)
+                self.comboBox.setVisible(False)
+                self.comboBox.setVisible(False)
+                self.horizontalSlider_2.setVisible(False)
+                self.pushButton_3.setVisible(False)
+                self.comboBox_5.setVisible(True)
+                self.horizontalSlider.setVisible(True)
 
+            elif(self.action_val == "Filtrage"):
+                self.setPhoto(self.image)
+                self.comboBox.setVisible(True)
+                self.comboBox.setVisible(True)
+                self.comboBox.setVisible(True)
+                self.horizontalSlider_2.setVisible(True)
+                self.pushButton_3.setVisible(True)
+                self.comboBox_5.setVisible(False)
+                self.horizontalSlider.setVisible(False)
+                self.pushButton_5.setVisible(False)
 
     def choiceMorph(self, value):
         self.morph_val = value
@@ -140,7 +220,7 @@ class Ui_MainWindow(object):
                 self.updateImage()
         else:
             self.comboBox_3.setVisible(False)
-        print(value)
+     
         
 
     def choiceKernel(self, value):
@@ -148,16 +228,23 @@ class Ui_MainWindow(object):
         if(self.kernel_val!=None and self.kernel_val!="Selectionner un noyau"):
             if(self.morph_val != None) and (self.morph_val != "Selectionner une morphologie"):
                 self.updateImage()
-        print(value)
+    
 
     def loadImage(self):
         # pour télécharger l'image sélectionnée
         self.filename = QFileDialog.getOpenFileName(filter="Image (*.*)")[0]
         self.image = cv.imread(self.filename,cv.IMREAD_UNCHANGED)
+        self.original_img = self.image
+        self.pushButton_4.setVisible(True)
         self.setPhoto(self.image)
 
     def saveImage(self):
         cv.imwrite(self.filename,self.tmp)
+
+    
+    def rechargeOriginalImg(self):
+        self.image = self.original_img
+        self.setPhoto(self.image)
 
 
     def setPhoto(self,image):
@@ -168,21 +255,30 @@ class Ui_MainWindow(object):
         image = QImage(frame, frame.shape[1],frame.shape[0], frame.strides[0], QImage.Format_RGB888)
         self.label.setPixmap(QtGui.QPixmap.fromImage(image))
 
+    def confirmeBinarization(self):
+        self.image = self.tmp
+        self.binarisation_val = None
+        self.comboBox_5.setCurrentIndex(0)
+        # self.horizontalSlider.setSliderPosition(0)
+
     def confirmeFiltre(self):
         self.image = self.tmp
         self.comboBox.setCurrentIndex(0)
+        self.comboBox_2.setCurrentIndex(0)
+        self.comboBox_3.setCurrentIndex(0)
         self.comboBox_2.setVisible(False)
         self.comboBox_3.setVisible(False)
+        self.filter_val = None
+        self.morph_val = None 
+        self.kernel_val = None
         self.horizontalSlider_2.setSliderPosition(0)
 
     def th_value(self,value):
         self.threshold = int((value*255)/100)
-        print("threshold : ",self.threshold)
         self.updateImage()
 
     def filter_value(self):
         self.blur = self.horizontalSlider_2.value()
-        print("size of filter : ",self.horizontalSlider_2.value())
         self.updateImage()
 
 
@@ -190,6 +286,24 @@ class Ui_MainWindow(object):
         imgRes = np.zeros(img.shape,np.uint8)
         cv.threshold(img,value,255,0,imgRes)
         return imgRes
+
+    def changeSauvola(self,img,size):
+        if (size % 2 != 0):
+            thresh_sauvola = threshold_sauvola(img, window_size=size)
+        else:
+           thresh_sauvola = threshold_sauvola(img, window_size=size+1)
+
+        binary_sauvola = img > thresh_sauvola
+        binary_sauvola = img_as_ubyte(binary_sauvola)
+        return binary_sauvola
+
+    
+
+
+    def changeOTSU(self,img):
+        binary_global = img > threshold_otsu(img)
+        binary_global = img_as_ubyte(binary_global)
+        return binary_global
 
     def filtreMoyen(self,img,size):
         kernel_size = (size+1 , size+1)
@@ -248,24 +362,25 @@ class Ui_MainWindow(object):
             filtredImage = cv.morphologyEx(image,cv.MORPH_TOPHAT,noyau)
         return filtredImage
 
-    def binary_otsus(image, filter):
-        #filtre en entré des valeur +1
-     """Binarization de l'image  avec Otsu's Binarization"""
-     image = cv.cvtColor(image,cv.IMREAD_GRAYSCALE)
-    #convertir vers grayscale
-     if len(image.shape) == 3:
-        gray_img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-     else:
-        gray_img = image
+    # def binary_otsus(image, filter):
+    #     #filtre en entré des valeur +1
+    #  """Binarization de l'image  avec Otsu's Binarization"""
+    #  image = cv.cvtColor(image,cv.IMREAD_GRAYSCALE)
+    # #convertir vers grayscale
+    #  if len(image.shape) == 3:
+    #     gray_img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    #  else:
+    #     gray_img = image
 
-    
-     if filter != 0:
-        blur = cv.GaussianBlur(gray_img, (3,3), 0)
-        otsu_img = cv.threshold(blur, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
-     else:
-        otsu_img = cv.threshold(gray_img, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
-    
-     return otsu_img
+    #  if filter != 0:
+    #     blur = cv.GaussianBlur(gray_img, (3,3), 0)
+    #     otsu_img = cv.threshold(blur, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
+    #  else:
+    #     otsu_img = cv.threshold(gray_img, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
+    #  return otsu_img
+
+
+
     def updateImage(self):
         if self.filter_val != "Selectionner un filtre":
             if self.filter_val == "filtre moyen":
@@ -287,7 +402,23 @@ class Ui_MainWindow(object):
                 if(self.morph_val!=None and self.morph_val!="Selectionner une morphologie") and (self.kernel_val!=None and self.kernel_val!="Selectionner un noyau"):
                     img = self.filtreMorpho(self.image,self.morph_val,self.kernel_val,self.blur)
                     self.setPhoto(img)
-           
+        
+        if self.binarisation_val != "Selectionner une technique de binarisation":
+            if self.binarisation_val == "Threshold":
+                img = cv.cvtColor(self.image,cv.COLOR_BGR2GRAY)
+                img = self.changeTh(img,self.threshold)
+                self.setPhoto(img)
+            if self.binarisation_val == "Sauvola":
+                img = cv.cvtColor(self.image,cv.COLOR_BGR2GRAY)
+                img = self.changeSauvola(img,self.threshold)
+                self.setPhoto(img)
+        
+            if self.binarisation_val == "OTSU":
+                img = cv.cvtColor(self.image,cv.COLOR_BGR2GRAY)
+                img = self.changeOTSU(img)
+                self.setPhoto(img)
+                
+                
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -296,7 +427,8 @@ class Ui_MainWindow(object):
         self.pushButton.setText(_translate("MainWindow", "Ouvrir une image"))
         self.pushButton_2.setText(_translate("MainWindow", "Sauvgarder l\'image"))
         self.pushButton_3.setText(_translate("MainWindow", "Confirmer le filtre"))
-
+        self.pushButton_4.setText(_translate("MainWindow", "Recharger l'image originale"))
+        self.pushButton_5.setText(_translate("MainWindow", "Confirmer la binarisation"))
 
 if __name__ == "__main__":
     import sys
